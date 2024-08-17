c32753478d3c15c338e4f1c52c5bfb4b7b834034
Md Arif Hassan <32775762+mdarifhassan@users.noreply.github.com>
2024-08-17 14:20:37 +1000

    Update Sidebar.js

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -1,29 +1,102 @@
-import React from "react";
+// Sidebar.js
+import React, { useState } from "react";
 import "./Sidebar.css";
-//import TwitterIcon from "@mui/icons-material/Twitter";
-import SidebarOption from "./SidebarOption";
-import HomeIcon from "@mui/icons-material/Home";
-import SearchIcon from "@mui/icons-material/Search";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
-import ListAltIcon from "@mui/icons-material/ListAlt";
-import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import TwitterIcon from "./img/uts_logo.png"
+import TwitterIcon from "./images/uts_logo.png";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import EditIcon from "@mui/icons-material/Edit";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 function Sidebar() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [profileInfo, setProfileInfo] = useState({
+    name: "John Doe", // Default name
+    profilePicture: TwitterIcon, // Default profile picture
+  });
+
+  const handleEditProfile = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveProfile = (newName, newProfilePicture) => {
+    setProfileInfo({
+      name: newName,
+      profilePicture: newProfilePicture,
+    });
+    setIsEditing(false);
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+  };
+
+  const handleUpdatePicture = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const newProfilePicture = reader.result;
+        setProfileInfo((prevProfileInfo) => ({
+          ...prevProfileInfo,
+          profilePicture: newProfilePicture,
+        }));
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="sidebar">
-      <img src={TwitterIcon} className="sidebar__twitterIcon" alt="Twitter Icon" />
-      <SidebarOption active Icon={HomeIcon} text="Home" />
-      <SidebarOption Icon={SearchIcon} text="Explore" />
-      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
-      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
-      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
-      <SidebarOption Icon={ListAltIcon} text="Lists" />
-      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
-      <SidebarOption Icon={MoreHorizIcon} text="More" />
+      {/* Profile Picture */}
+      <label htmlFor="profile-picture-input">
+        <div className="profile-picture">
+          <img src={profileInfo.profilePicture} alt="Profile" />
+        </div>
+      </label>
+      <input
+        id="profile-picture-input"
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}
+        onChange={handleUpdatePicture}
+      />
+
+      {/* Display Name */}
+      <p>{profileInfo.name}</p>
+
+      {/* Sidebar Menu */}
+      <div className="sidebar-menu">
+        <p onClick={handleEditProfile}>
+          <AccountCircleIcon /> Profile
+        </p>
+        <p>
+          <EditIcon /> Edit
+        </p>
+        <p>
+          <SettingsIcon /> Settings
+        </p>
+        <p>
+          <LogoutIcon /> Logout
+        </p>
+      </div>
+
+      {/* Edit Profile Modal */}
+      {isEditing && (
+        <div className="edit-profile-modal">
+          <div className="modal-content">
+            <h2>Edit Profile</h2>
+            <label htmlFor="new-name">New Name:</label>
+            <input
+              type="text"
+              id="new-name"
+              value={profileInfo.name}
+              onChange={(e) => setProfileInfo((prev) => ({ ...prev, name: e.target.value }))}
+            />
+            <button onClick={() => handleSaveProfile(profileInfo.name, profileInfo.profilePicture)}>Save</button>
+            <button onClick={handleCancelEdit}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
